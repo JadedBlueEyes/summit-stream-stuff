b6df1c34a4b37e279c571936239e8aa54a827f9a
Jade Ellis <jade@ellis.link>
2025-09-15 23:32:59 +0100

    Auto set config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [svelte(), wasm(), topLevelAwait()],
   build: {
     rollupOptions: {
